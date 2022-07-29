0ed8ca4529d5ff4f210490f8904890f7f247642a
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2022-07-29 23:58:02 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+echo '<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.responsive {
+  width: 100%;
+  height: auto;
+}
+</style>
+</head>
+<body style="background-color:black;">
+
+<img src="solar-system-9.png" alt="Nature" class="responsive" width="600" height="400">
+
+</body>
+</html>';
