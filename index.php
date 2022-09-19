d234eaef6ee174022ef76f782aeb1daecb66eaf7
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2022-09-19 22:33:28 +0530

    spin css

--- index.php
+++ index.php
@@ -4,14 +4,39 @@ echo '<!DOCTYPE html>
    <head>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <style>
-         .responsive {
-         width: 100%;
-         height: auto;
-         }
+		#solar-system {
+		   background: url("https://raw.githubusercontent.com/sidd-harth/planets/main/images/9.png")   center center;
+           background-repeat: no-repeat;			  
+		   background-size: cover;
+		   content: "";
+		   position: static;
+		   animation: spin 25s linear infinite;
+		   width: 50vw;
+		   height: 50vw;
+		}
+
+		@keyframes spin {
+		  100% { transform: rotate(360deg); }
+		}
+		
+		body {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			background: url("https://raw.githubusercontent.com/sidd-harth/planets/main/images/bg.png");
+		}
+		
+/* 		.shadow {
+			  animation: rainbow 2s linear infinite;
+			} */
+
+
       </style>
    </head>
-   <body style="background-color:black;">
-      <img src="images/solar-system-9.png" alt="Nature" class="responsive" width="600" height="400">
+   <body>
+	   
+				 <div id="solar-system"> </div>
+
    </body>
 </html>
 ';
