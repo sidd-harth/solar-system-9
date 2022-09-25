8c38fd9a6a664e81d123d040acd564bbcf8e712e
siddharth <sid@admin.com>
2022-09-25 12:39:34 +0000

    Update 'pr.sh'

--- pr.sh
+++ pr.sh
@@ -1,14 +1,9 @@
 echo "Opening a Pull Request"
 
-echo "Script: - Build number: $BUILD_NUMBER"
-echo "Script: - Token $ARGOCD_TOKEN"
-echo "authorization: $ARGOCD_TOKEN" 
- 
-
 curl -X 'POST' \
   'http://139.59.21.103:3000/api/v1/repos/siddharth/gitops-argocd/pulls' \
   -H 'accept: application/json' \
-  -H 'authorization: $ARGOCD_TOKEN' \
+  -H "authorization: $ARGOCD_TOKEN" \
   -H 'Content-Type: application/json' \
   -d '{
   "assignee": "siddharth",
